adc72ab26395e7b81bf62126f2968e1ebe39efd6
Julie Jonak <juliejonak10@gmail.com>
2019-07-26 10:11:08 -0500

    Adds append() to RingBuffer and passes all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,8 +7,17 @@ class RingBuffer:
   def append(self, item):
     # If storage is full, overwrite the oldest
     # Decide by comparing capacity to current
+    if self.current == len(self.storage):
+      print("Ring Buffer is full. Overwriting oldest item.")
+      # Reset current to start of the list
+      self.current = 0
+      self.storage[self.current] = item
+      self.current += 1
 
     # Else, add to the end of storage and increase current
+    else:
+      self.storage[self.current] = item
+      self.current += 1
 
   def get(self):
     # Create new list that filters out any None values
