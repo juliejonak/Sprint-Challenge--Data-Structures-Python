1f3911daf3cc1aa798e4f3fb282e984d9214c8d7
Julie Jonak <juliejonak10@gmail.com>
2019-07-26 10:08:48 -0500

    Adds get() method to RingBuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,12 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # If storage is full, overwrite the oldest
+    # Decide by comparing capacity to current
+
+    # Else, add to the end of storage and increase current
 
   def get(self):
-    pass
+    # Create new list that filters out any None values
+    get_list = list(filter(None, self.storage))
+    return get_list
